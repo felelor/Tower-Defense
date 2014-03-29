46dbde5ac84a5660c335a375de5963f23f722e72
felelor <jshepard@nd.edu>
2014-03-28 23:23:43 -0400

    Create oldmain.cpp

---
+++ oldmain.cpp
@@ -0,0 +1,12 @@
+#include "sprite.h"
+//#include "Tower.h"
+#include <SDL/SDL.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+	
+	sprite tester;
+	tester.play();
+	return 0;
+}
